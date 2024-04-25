2dc7f2a941e647fe52cb6d31d95ff6e7eb6ea721
NicoloTrevisan <trevisannicolo@gmail.com>
2024-04-25 11:58:23 +0200

    Update table_components.py

--- table_components.py
+++ table_components.py
@@ -1,20 +1,20 @@
-# REMINDER: Before running this script, activate the virtual environment with:
+# REMINDER: activate the virtual environment with (done by the .sh script, keeping it here for reference)
 # source /Users/nico/Desktop/Google_Drive/MAGMA/PCA_ICA_April2024/IC_loci_GWAS_Catalog/HTML_table/venv/bin/activate
 
 
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
-# Load the CSV file into a DataFrame
+# Load the CSV file 
 data = pd.read_csv('/Users/nico/Desktop/Google_Drive/MAGMA/PCA_ICA_April2024/IC_loci_GWAS_Catalog/all_combined_traits.csv')
 
-# Process the 'Component' column to keep only the "IC" part (e.g., "IC1", "IC2", etc.)
+# keep only the "IC" part ("IC1", "IC2", etc.)
 data['Component'] = data['Component'].str.extract('(IC\\d+)')
 
 # Group the data by 'Component' and create a dictionary of traits, chromosomes, and positions
 grouped_data = data.groupby('Component').apply(lambda x: x[['Chromosome', 'Position', 'Trait']].to_dict('records')).to_dict()
 
-# Set up the environment for Jinja2
+# environment for Jinja2
 env = Environment(loader=FileSystemLoader(searchpath=''))
 template = env.get_template('index.html')  # You need to create a template.html file
 
